b2d6c9cd30f302347fd585c5b9b412974e4b43b6
garrypas <garry.passarella@gmail.com>
2024-01-11 17:19:36 +0000

    Added invite enums

---
+++ src/invite/invite.enums.ts
@@ -0,0 +1,5 @@
+export enum InviteEnums {
+  pending = 'pending',
+  viewed = 'viewed',
+  dismissed = 'dismissed',
+}
--- src/listing/listing.rules.ts
+++ src/listing/listing.rules.ts
@@ -2,4 +2,4 @@ import { ListingEnums } from "./listing.enums";
 
 export const canRelist = (listingStatus: ListingEnums) => {
   return listingStatus === ListingEnums.expired;
-}
+};
